88de52237a1e25827c45c8484e6afb46cea2d7e8
Johntae Stroud <johntae.stroud@gmail.com>
2018-04-01 14:18:47 -0400

    created bitcoinmarket class

---
+++ crypto/src/models/bitcoin-market.class.ts
@@ -0,0 +1,21 @@
+export class BitcoinMarket {
+  public bitcoin_percentage_of_market_cap: number;
+  public active_cryptocurrencies: number;
+  public total_volume_usd: number;
+  public active_markets: number;
+  public total_market_cap_by_available_supply_usd: number;
+
+  constructor(data?: any) {
+    const defaults = {
+    
+        ...data //unwrapping data
+      
+    };
+
+    this.active_cryptocurrencies = defaults.active_cryptocurrencies;
+    this.active_markets = defaults.active_markets;
+    this.total_volume_usd = defaults.total_volume_usd;
+    this.total_market_cap_by_available_supply_usd = defaults.total_market_cap_by_available_supply_usd;
+    this.bitcoin_percentage_of_market_cap = defaults.bitcoin_percentage_of_market_cap;
+  }
+}

0dbc9a2e47db6acea8e18467e13fea0e0cce4279
Johntae Stroud <johntae.stroud@gmail.com>
2018-04-03 17:44:58 -0400

    working with services by adding a class

--- crypto/src/app/app.component.ts
+++ crypto/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CryptoService } from '../services/crypto.service';
+import { BitcoinMarket} from '../models/bitcoin-market.class';
 
 @Component({
   selector: 'app-root',
@@ -7,13 +8,18 @@ import { CryptoService } from '../services/crypto.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  //creating a prop for the class { BitcoinMarket }
+  public bitcoinMarketCap: BitcoinMarket = new BitcoinMarket();
+
   constructor(public cryptoService: CryptoService) {
     this.getBitcoinStats(); //calling a reference 
   }
 
   public getBitcoinStats(): void {
     this.cryptoService.getBitcoinmarketCap().subscribe((data: any) => {
-      console.log(data);
+      this.bitcoinMarketCap = new BitcoinMarket(data); //passing our data
+      console.log(this.bitcoinMarketCap);
     });
   }
 }

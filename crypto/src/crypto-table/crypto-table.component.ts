62abef175dd3e46abdf10ccba4b34d929da777e6
Johntae Stroud <johntae.stroud@gmail.com>
2018-04-04 17:04:00 -0400

    table sorting

--- crypto/src/crypto-table/crypto-table.component.ts
+++ crypto/src/crypto-table/crypto-table.component.ts
@@ -11,6 +11,7 @@ import { Component } from '@angular/core';
 export class CryptoTableComponent {
 
   public top100Cryptos: CryptoCurrency[]; //assign it (CryptoCurrency) as a type
+  public sortValues: any = { rank: false, marketCap: false, volume: false, change24: false, price: false };//keeping track of what our values are in the header (descending/ascending)
 
   constructor(public cryptoService: CryptoService) {
     this.getTop100Cryptos();
@@ -21,7 +22,18 @@ export class CryptoTableComponent {
       this.top100Cryptos = data.map((element: any) => {
         return new CryptoCurrency(element);
       });
-      console.log(this.top100Cryptos);
     });
+  }
+
+  public sortNumeric(sortValue: boolean, key: string) { // using a key because we want this to be dynamic enough that we can sort by any of the numeric values
+    if(sortValue) {
+      this.top100Cryptos = this.top100Cryptos.sort((a: CryptoCurrency, b: CryptoCurrency) => {
+          return a[key] - b[key];
+      });
+    } else {
+      this.top100Cryptos = this.top100Cryptos.sort((a: CryptoCurrency, b: CryptoCurrency) => {
+        return b[key] - a[key];
+    });
+    }
   }
 }
